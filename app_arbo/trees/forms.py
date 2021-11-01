b04a73cd334dacbe4ed691802f4b59c9ab14333a
proj-int01 <adilson.monte.alto@gmail.com>
2021-11-01 16:18:06 -0300

    pause

---
+++ app_arbo/trees/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf.file import FileAllowed, FileField, FileRequired
+from wtforms import Form,IntegerField,StringField,BooleanField,TextAreaField,validators
+
+
+class Addtree(Form):
+    name = StringField('Nome: ', [validators.DataRequired()])
+    species = StringField('Espécie: ', [validators.DataRequired()])
+    qtd = IntegerField('Quantidade: ', [validators.DataRequired()])
+    origin = StringField('Família: ', [validators.DataRequired()])
+    description = TextAreaField('Descrição: ', [validators.DataRequired()])
+
+    image_1 = FileField('Imagem 1: ', validators=[FileRequired(),FileAllowed(['jpg', 'png', 'git', 'jpeg'])])
+    image_2 = FileField('Imagem 2: ', validators=[FileRequired(),FileAllowed(['jpg', 'png', 'git', 'jpeg'])])
+    
+
+
+
+class Addgrove2(Form): # para cadastrar imagem das Praças
+    name = StringField('Nome: ', [validators.DataRequired()])
+    adress = StringField('Endereço: ', [validators.DataRequired()])
+    description = TextAreaField('Descrição: ', [validators.DataRequired()])
+
+    image_1 = FileField('Imagem 1: ', validators=[FileRequired(),FileAllowed(['jpg', 'png', 'git', 'jpeg'])])
+    image_2 = FileField('Imagem 2: ', validators=[FileRequired(),FileAllowed(['jpg', 'png', 'git', 'jpeg'])])
+    image_3 = FileField('Imagem 3: ', validators=[FileRequired(),FileAllowed(['jpg', 'png', 'git', 'jpeg'])])
+    image_4 = FileField('Imagem 4: ', validators=[FileRequired(),FileAllowed(['jpg', 'png', 'git', 'jpeg'])])
--- app_arbo/trees/rotas.py
+++ app_arbo/trees/rotas.py
@@ -1,8 +1,9 @@
 from flask import redirect , render_template, url_for, flash, request
-
+from .forms import Addtree, Addgrove2
 from app_arbo import db, app
 from .models import Family, Grove 
-@app.route('/addfamily', methods=['GET', 'POST'])
+
+@app.route('/addfamilia', methods=['GET', 'POST'])
 def addfamily():
     
     if request.method == "POST":
@@ -12,7 +13,7 @@ def addfamily():
         db.session.commit()
         flash(f'A family {getfamily} foi cadastrada com sucesso', 'success')
         return redirect(url_for('addfamily'))
-    return render_template('/trees/addfamily.html', family='family')
+    return render_template('/trees/addfamilia.html', familia='familia')
 
 
 
@@ -23,10 +24,21 @@ def addgrove():
     
     if request.method == "POST":
         getgrove = request.form.get('grove')
-        grove = Grove (name=getgrove)
+        grove = Grove (name=getgrove,address=getgrove)
         db.session.add(grove)
         db.session.commit()
         flash(f'A Praça {getgrove} foi cadastrada com sucesso', 'success')
         return redirect(url_for('addgrove'))
     return render_template('/trees/addfamily.html')
 
+
+@app.route('/addarvore', methods=['GET', 'POST'])
+def addtree():
+    form = Addtree(request.form)
+    return render_template('/trees/addarvore.html', title="Cadastrar Árvores", form=form)
+
+
+@app.route('/addpraca', methods=['GET', 'POST'])
+def addgrove2():
+    form = Addgrove2(request.form)
+    return render_template('/trees/addpraca.html', title="Cadastrar Praças/Fotos", form=form)
