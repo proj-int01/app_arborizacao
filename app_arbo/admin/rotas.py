c85b5d0b469e7f0d580c98f8c4056b798869a7bf
proj-int01 <adilson.monte.alto@gmail.com>
2021-11-02 21:54:24 -0300

    teste Ok

--- app_arbo/admin/rotas.py
+++ app_arbo/admin/rotas.py
@@ -1,6 +1,7 @@
 from flask import render_template, session, request, redirect, url_for, flash
-
+from app_arbo.trees.models import Addarvore, Praca, Familia, Arvore
 from app_arbo import app, db, bcrypt
+
 from .models import User 
 from .forms import RegistrationForm, LoginFormulario
 import os
@@ -9,9 +10,36 @@ import os
 @app.route('/admin')
 def admin():
     if 'email' not in session:
-        flash(f'Favor fazer seu login no sistema.', 'success')
+        flash(f'Favor faça seu login no sistema.', 'success')
         return redirect(url_for('login'))
-    return render_template('admin/index.html', title='Página Administrativa')
+    arvores = Addarvore.query.all()
+    return render_template('admin/index.html', title='Página Administrativa', arvores=arvores)
+
+@app.route('/pracas')
+def pracas():
+    if 'email' not in session:
+        flash(f'Favor faça seu login no sistema.', 'success')
+        return redirect(url_for('login'))
+    pracas = Praca.query.order_by(Praca.id.desc()).all()
+    return render_template('admin/pracas.html', title='Página Praças', pracas=pracas)
+
+@app.route('/arvores')
+def arvores():
+    if 'email' not in session:
+        flash(f'Favor faça seu login no sistema.', 'success')
+        return redirect(url_for('login'))
+    arvores = Arvore.query.order_by(Arvore.id.desc()).all()
+    return render_template('admin/arvores.html', title='Página Árvores', arvores=arvores)
+
+@app.route('/familias')
+def familias():
+    if 'email' not in session:
+        flash(f'Favor faça seu login no sistema.', 'success')
+        return redirect(url_for('login'))
+    familias = Familia.query.order_by(Familia.id.desc( )).all()
+    return render_template('admin/familia.html', title='Página Famílias', familias=familias)
+
+
 
 
 
--- app_arbo/trees/rotas.py
+++ app_arbo/trees/rotas.py
@@ -1,4 +1,4 @@
-from flask import redirect , render_template, url_for, flash, request
+from flask import redirect , render_template, url_for, flash, request, session
 
 from .forms import Addpraca, Addtree 
 from app_arbo import db, app
@@ -10,27 +10,33 @@ def addfamilia():
         getfamilia = request.form.get('familia')
         familia = Familia (name=getfamilia)
         db.session.add(familia)
-        flash (f'A familia {getfamilia} foi cadastrada com sucesso', 'success')
+        flash (f'A família {getfamilia} foi cadastrada com sucesso', 'success')
         db.session.commit()
         return redirect(url_for('addfamilia'))
     return render_template('/trees/cadarvore.html')
 
 
 @app.route('/cadarvore', methods=['GET', 'POST'])
-def cadarvore():    
+def cadarvore():
+    if 'email' not in session:
+        flash(f'Favor faça seu login no sistema.', 'success')
+        return redirect(url_for('login'))    
     if request.method == "POST":
         getarvore = request.form.get('arvore')
         getespecie= request.form.get('especie')
         arvore = Arvore(name=getarvore,especie=getespecie)
         db.session.add(arvore)
         db.session.commit()
-        flash(f'A Aravore {getarvore} foi cadastrada com sucesso', 'success')
+        flash(f'A Árvore {getarvore} foi cadastrada com sucesso', 'success')
         return redirect(url_for('cadarvore'))
     return render_template('/trees/cadarvore.html', arvore='arvore')
 
 
 @app.route('/addarvore', methods=['GET', 'POST'])
 def addarvore():
+    if 'email' not in session:
+        flash(f'Favor faça seu login no sistema.', 'success')
+        return redirect(url_for('login'))
     arvores = Arvore.query.all()
     familias = Familia.query.all()
     pracas = Praca.query.all()
@@ -44,7 +50,7 @@ def addarvore():
 
         addarv = Addarvore(qtd=qtd,origem=origem, arvore_id=arvore,familia_id=familia, praca_id=praca)
         db.session.add(addarv)
-        flash(f'A Arvore foi cadastrada com sucesso', 'success')
+        flash(f'A Árvore foi cadastrada com sucesso', 'success')
         db.session.commit()
         return redirect(url_for('admin'))
        
@@ -53,6 +59,9 @@ def addarvore():
 
 @app.route('/addpraca', methods=['GET', 'POST'])
 def addpraca():
+    if 'email' not in session:
+        flash(f'Favor faça seu login no sistema.', 'success')
+        return redirect(url_for('login'))
     if request.method == "POST":
         getpraca = request.form.get('praca')
         getend = request.form.get('end')
