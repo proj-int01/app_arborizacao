90852886956cfe92b6af61a84aa4e59fc274a513
proj-int01 <adilson.monte.alto@gmail.com>
2021-11-06 03:53:19 -0300

    Teste OK

--- app_arbo/trees/rotas.py
+++ app_arbo/trees/rotas.py
@@ -17,7 +17,7 @@ def addfamilia():
         flash (f'A família {getfamilia} foi cadastrada com sucesso', 'success')
         db.session.commit()
         return redirect(url_for('addfamilia'))
-    return render_template('/trees/cadarvore.html')
+    return render_template('/trees/cadarvore.html', title='Adicionar Famílias')
 
 @app.route('/updatefamilia/<int:id>', methods=['GET','POST'])
 def updatefamilia(id):
@@ -35,24 +35,6 @@ def updatefamilia(id):
 
 
 
-@app.route('/cadarvore', methods=['GET', 'POST'])
-def cadarvore():
-    if 'email' not in session:
-        flash(f'Favor faça seu login no sistema.', 'success')
-        return redirect(url_for('login'))  
-
-    if request.method == "POST":
-        getarvore = request.form.get('arvore')
-        getespecie= request.form.get('especie')
-        arvore = Arvore(name=getarvore,especie=getespecie)
-        db.session.add(arvore)
-        db.session.commit()
-        flash(f'A Árvore {getarvore} foi cadastrada com sucesso', 'success')
-        return redirect(url_for('cadarvore'))
-    return render_template('/trees/cadarvore.html', arvore='arvore')
-
-
-
 @app.route('/updatearvore/<int:id>', methods=['GET', 'POST'])
 def updatearvore(id):
     if 'email' not in session:
@@ -67,7 +49,7 @@ def updatearvore(id):
         flash (f'A Árvore foi atualizada com sucesso', 'success')
         db.session.commit()
         return redirect(url_for('arvores'))
-    return render_template('/trees/updatearvore.html', updatearvore=updatearvore)
+    return render_template('/trees/updatearvore.html', title='Atualizar Árvore', updatearvore=updatearvore)
 
 
 
@@ -84,17 +66,69 @@ def addarvore():
     if request.method=="POST":
         arvore = request.form.get('arvore')
         familia = request.form.get('familia')
-        praca = request.form.get('praca')
+        pracas = request.form.get('praca')
         qtd = form.qtd.data
         origem = form.origem.data
 
-        addarv = Addarvore(qtd=qtd,origem=origem, arvore_id=arvore,familia_id=familia, praca_id=praca)
+        addarv = Addarvore(qtd=qtd,origem=origem, arvore_id=arvore,familia_id=familia, praca_id=pracas)
         db.session.add(addarv)
         flash(f'A Árvore foi cadastrada com sucesso', 'success')
         db.session.commit()
         return redirect(url_for('admin'))
        
     return render_template('/trees/addarvore.html', title="Cadastrar Árvores", form=form, arvores=arvores, familias=familias, pracas=pracas)
+
+
+
+
+@app.route('/updateaddarvore/<int:id>', methods=['GET', 'POST'])
+def updateaddarvore(id):
+    if 'email' not in session:
+        flash(f'Favor faça seu login no sistema.', 'success')
+        return redirect(url_for('login'))
+    
+    form = Addtree(request.form)
+    arvores = Arvore.query.all()
+    familias = Familia.query.all()
+    pracas = Praca.query.all()
+    add_arvore = Addarvore.query.get_or_404(id)
+    arvore = request.form.get('arvore')
+    familia = request.form.get('familia')
+    praca = request.form.get('praca')
+    
+    if request.method=="POST":
+        add_arvore.praca_id = praca
+        add_arvore.familia_id = familia
+        add_arvore.arvore_id = arvore
+        add_arvore.qtd = form.qtd.data
+        add_arvore.origem = form.origem.data
+
+        db.session.commit()
+        flash(f'A relação foi atualizada com sucesso', 'success')
+        return redirect(url_for('admin'))
+
+    form.qtd.data = add_arvore.qtd
+    form.origem.data = add_arvore.origem
+    
+  
+       
+        
+       
+    return render_template('/trees/updateaddarvore.html', title="Cadastrar Árvores", form=form, arvores=arvores, familias=familias, pracas=pracas, add_arvore=add_arvore)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @app.route('/addpraca', methods=['GET', 'POST'])
@@ -110,7 +144,7 @@ def addpraca():
         db.session.commit()
         flash(f'A Praça {getpraca} foi cadastrada com sucesso', 'success')
         return redirect(url_for('addpraca'))
-    return render_template('/trees/addpraca.html')
+    return render_template('/trees/addpraca.html', title='Cadastrar Praças')
     
 
 @app.route('/updatepraca/<int:id>', methods=['GET', 'POST'])
@@ -127,12 +161,31 @@ def updatepraca(id):
         flash (f'A Praca foi atualizada com sucesso', 'success')
         db.session.commit()
         return redirect(url_for('pracas'))
-    return render_template('/trees/updatepraca.html', updatepraca=updatepraca)
+    return render_template('/trees/updatepraca.html', title='Atualizar praça',updatepraca=updatepraca)
 
 
 
 
 
+
+
+
+
+@app.route('/cadarvore', methods=['GET', 'POST'])
+def cadarvore():
+    if 'email' not in session:
+        flash(f'Favor faça seu login no sistema.', 'success')
+        return redirect(url_for('login'))  
+
+    if request.method == "POST":
+        getarvore = request.form.get('arvore')
+        getespecie= request.form.get('especie')
+        arvore = Arvore(name=getarvore,especie=getespecie)
+        db.session.add(arvore)
+        db.session.commit()
+        flash(f'A Árvore {getarvore} foi cadastrada com sucesso', 'success')
+        return redirect(url_for('cadarvore'))
+    return render_template('/trees/cadarvore.html', title='Cadastrar Árvores', arvore='arvore')
 
 
 
